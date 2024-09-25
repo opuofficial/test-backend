b1338bb151a7412bd1c7ac6c5cfda3ed73ec6e2c
Opu <opuofficial24@gmail.com>
2024-09-26 03:46:35 +0600

    morgan middleware added

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 9000;
 const cors = require("cors");
+const morgan = require("morgan");
 
 app.use(cors());
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
